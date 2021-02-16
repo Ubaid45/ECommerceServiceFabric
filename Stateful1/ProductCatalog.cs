13279add6f51d2fa1e4fb81dbed98e04660b0fcc
Ubaid Rana <ubaidrana45@gmail.com>
2021-02-16 22:39:51 +0100

    Added stateful project

--- Stateful1/ProductCatalog.cs
+++ Stateful1/ProductCatalog.cs
@@ -4,20 +4,36 @@ using System.Fabric;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using ECommerce.ProductCatalog.Model;
+using ECommerce.ProductCatalog.Models;
+using Microsoft.ServiceFabric.Data;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.V2.FabricTransport.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 
-namespace Stateful1
+namespace ECommerce.ProductCatalog
 {
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class Stateful1 : StatefulService
+    internal sealed class ProductCatalog : StatefulService, IProductCatalogService
     {
-        public Stateful1(StatefulServiceContext context)
+        private IProductRepository _repo;
+
+        public ProductCatalog(StatefulServiceContext context)
             : base(context)
         { }
+
+        public async Task AddProductAsync(Product product)
+        {
+            await _repo.AddProduct(product);
+        }
+
+        public async Task<Product[]> GetAllProductsAsync()
+        {
+            return (await _repo.GetAllProducts()).ToArray();
+        }
 
         /// <summary>
         /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
@@ -26,9 +42,14 @@ namespace Stateful1
         /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
         /// </remarks>
         /// <returns>A collection of listeners.</returns>
-        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        protected override IEnumerable<ServiceReplicaListener>
+           CreateServiceReplicaListeners()
         {
-            return new ServiceReplicaListener[0];
+            return new[]
+            {
+            new ServiceReplicaListener(context =>
+               new FabricTransportServiceRemotingListener(context, this))
+         };
         }
 
         /// <summary>
@@ -38,31 +59,41 @@ namespace Stateful1
         /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
         protected override async Task RunAsync(CancellationToken cancellationToken)
         {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
+            _repo = new ServiceFabricProductRepository(this.StateManager);
 
-            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
-
-            while (true)
+            var product1 = new Product
             {
-                cancellationToken.ThrowIfCancellationRequested();
+                Id = Guid.NewGuid(),
+                Name = "Dell Monitor",
+                Description = "Computer Monitor",
+                Price = 500,
+                Availability = 100
+            };
 
-                using (var tx = this.StateManager.CreateTransaction())
-                {
-                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
+            var product2 = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Surface Book",
+                Description = "Microsoft's Latest Laptop, i7 CPU, 1Tb SSD",
+                Price = 2200,
+                Availability = 15
+            };
 
-                    ServiceEventSource.Current.ServiceMessage(this.Context, "Current Counter Value: {0}",
-                        result.HasValue ? result.Value.ToString() : "Value does not exist.");
+            var product3 = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Arc Touch Mouse",
+                Description = "Computer Mouse, bluetooth, requires 2 AAA batteries",
+                Price = 60,
+                Availability = 30
+            };
 
-                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
+            await _repo.AddProduct(product1);
+            await _repo.AddProduct(product2);
+            await _repo.AddProduct(product3);
 
-                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
-                    // discarded, and nothing is saved to the secondary replicas.
-                    await tx.CommitAsync();
-                }
+            IEnumerable<Product> all = await _repo.GetAllProducts();
 
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
-            }
         }
     }
 }
